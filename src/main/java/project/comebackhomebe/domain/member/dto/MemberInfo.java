27ec4641db121aab97c4bf2e774bbdcdcd146e0f
HyeonSeong <gus8131@naver.com>
2025-03-20 17:44:57 +0900

    feat : Oauth2 dto

--- src/main/java/project/comebackhomebe/domain/member/dto/MemberInfo.java
+++ src/main/java/project/comebackhomebe/domain/member/dto/MemberInfo.java
@@ -1,10 +1,19 @@
 package project.comebackhomebe.domain.member.dto;
 
+import project.comebackhomebe.domain.member.entity.Member;
 import project.comebackhomebe.domain.member.entity.Role;
 
 public record MemberInfo(
         String username,
-        Role role
+        Role role,
+        String kakao_id
 
 ) {
+    public static MemberInfo of(Member member) {
+        return new MemberInfo(
+                member.getUsername(),
+                member.getRole(),
+                member.getKakao_id()
+        );
+    }
 }
