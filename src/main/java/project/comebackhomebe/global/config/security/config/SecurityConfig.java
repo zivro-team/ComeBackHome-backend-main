97803b115a9d4e5e858168f16ae237364d91e107
HyeonSeong <gus8131@naver.com>
2025-03-20 16:20:31 +0900

    Feat : SecurityConfig - 계층권한 설정

---
+++ src/main/java/project/comebackhomebe/global/config/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package project.comebackhomebe.global.config.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // Spring Security에 대한 디버깅 모드를 사용하기 위한 어노테이션 (default : false)
+public class SecurityConfig {
+
+    // 계층 권한
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy(
+                """
+                        ADMIN > USER"""
+        );
+    }
+}
+
