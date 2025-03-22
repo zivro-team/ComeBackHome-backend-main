f46a7cf1c1a9b8efee2328f72045720b33eaea6b
HyeonSeong <gus8131@naver.com>
2025-03-22 13:50:22 +0900

    fix : 서비스 수정 - 레포에 회원 정보 저장

--- src/main/java/project/comebackhomebe/global/config/security/service/CustomOAuth2Service.java
+++ src/main/java/project/comebackhomebe/global/config/security/service/CustomOAuth2Service.java
@@ -1,5 +1,6 @@
 package project.comebackhomebe.global.config.security.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -11,12 +12,15 @@ import project.comebackhomebe.domain.member.dto.MemberInfo;
 import project.comebackhomebe.domain.member.dto.OAuth2Info;
 import project.comebackhomebe.domain.member.entity.Member;
 import project.comebackhomebe.domain.member.entity.Role;
+import project.comebackhomebe.domain.member.repository.MemberRepository;
 
 import java.util.Map;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class CustomOAuth2Service extends DefaultOAuth2UserService {
+    private final MemberRepository memberRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
@@ -46,6 +50,7 @@ public class CustomOAuth2Service extends DefaultOAuth2UserService {
 
         // 엔티티 저장
         Member member = Member.from(username, Role.USER, kakaoId);
+        memberRepository.save(member);
         log.info("[member] : {}", member);
 
         // DTO 넘기기
