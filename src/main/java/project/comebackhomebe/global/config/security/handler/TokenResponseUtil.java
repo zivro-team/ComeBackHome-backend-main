1589ff54b781bcf3f6de6e1ce972121787a89e20
HyeonSeong <gus8131@naver.com>
2025-03-24 17:22:10 +0900

    refactor : clean code - SRP - 메서드 정리

--- src/main/java/project/comebackhomebe/global/config/security/handler/TokenResponseUtil.java
+++ src/main/java/project/comebackhomebe/global/config/security/handler/TokenResponseUtil.java
@@ -2,52 +2,19 @@ package project.comebackhomebe.global.config.security.handler;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
 @Component
 @RequiredArgsConstructor
 public class TokenResponseUtil {
-
-//    private final RefreshRepository refreshRepository;
-//
-//    /**
-//     * RefreshRepo에 저장하기 위해 사용
-//     *
-//     * @param username
-//     * @param refresh
-//     * @param expiredMs
-//     */
-//    public void addRefreshEntity(String username, String refresh, Long expiredMs) {
-//
-//        Date date = new Date(System.currentTimeMillis() + expiredMs);
-//
-//        RefreshUser refreshEntity = new RefreshUser();
-//        refreshEntity.setUsername(username);
-//        refreshEntity.setRefreshToken(refresh);
-//        refreshEntity.setExpiration(date.toString());
-//
-//        refreshRepository.save(refreshEntity);
-//    }
-
-    /**
-     * key = 키 값 , value = jwt값
-     * Cookie 로 저장하기 위해 생성
-     *
-     * @param key
-     * @param value
-     * @return
-     */
     public Cookie createCookie(String key, String value) {
 
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(24 * 60 * 60);
-        //cookie.setSecure(true);
-        //cookie.setPath("/");
+        cookie.setSecure(true);
+        cookie.setPath("/");
         cookie.setHttpOnly(true);
 
         return cookie;
@@ -79,7 +46,5 @@ public class TokenResponseUtil {
     public Object getPrincipal(HttpServletRequest request) {
         return request.getUserPrincipal();
     }
-
-
 }
 
