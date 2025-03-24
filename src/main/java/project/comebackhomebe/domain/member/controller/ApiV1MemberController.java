adc691fbeac3dd79793e50193e8e6da762a2b337
HyeonSeong <gus8131@naver.com>
2025-03-24 17:32:12 +0900

    refactor : clean code - SRP - DI 정리

--- src/main/java/project/comebackhomebe/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/project/comebackhomebe/domain/member/controller/ApiV1MemberController.java
@@ -1,17 +1,15 @@
 package project.comebackhomebe.domain.member.controller;
 
-import co.elastic.clients.elasticsearch._types.Refresh;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import project.comebackhomebe.domain.member.service.MemberService;
-import project.comebackhomebe.global.config.security.handler.RefreshHandler;
-import project.comebackhomebe.global.config.security.jwt.JwtUtil;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import project.comebackhomebe.global.util.redis.RefreshTokenService;
 
 @RestController
@@ -20,30 +18,15 @@ import project.comebackhomebe.global.util.redis.RefreshTokenService;
 @Slf4j
 public class ApiV1MemberController {
 
-    private final MemberService memberService;
-    private final RefreshHandler refreshHandler;
     private final RefreshTokenService refreshTokenService;
-    private final JwtUtil jwtUtil;
 
-    @GetMapping("/main")
-    public String main(){
-        return "성공";
-    }
-
-    @PostMapping("/refresh")
-    public ResponseEntity<?> refresh(HttpServletRequest request, HttpServletResponse response) {
-        refreshHandler.refreshHandler(request, response);
-        return ResponseEntity.ok(HttpStatus.OK);
-    }
-
+    // access 토큰 재발급
     @PostMapping("/reissue")
-    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
-        String refresh = refreshTokenService.findRefreshToken(request);
-        response.setHeader("Authorization", jwtUtil.newGenerateToken(refresh));
-        log.info(jwtUtil.newGenerateToken(refresh));
-        return ResponseEntity.ok(HttpStatus.OK);
+    public void reissue(HttpServletRequest request, HttpServletResponse response) {
+        refreshTokenService.newCreateAccessToken(request, response);
     }
 
+    // 로그아웃
     @DeleteMapping("/logout")
     public void logout(HttpServletRequest request) {
         refreshTokenService.deleteRefreshToken(request);
