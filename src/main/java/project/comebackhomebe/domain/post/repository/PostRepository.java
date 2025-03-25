f681ae585972f0b889d0764d2f09b4ea61955263
HyeonSeong <gus8131@naver.com>
2025-03-25 18:44:31 +0900

    feat : post init

--- src/main/java/project/comebackhomebe/domain/post/repository/PostRepository.java
+++ src/main/java/project/comebackhomebe/domain/post/repository/PostRepository.java
@@ -2,6 +2,7 @@ package project.comebackhomebe.domain.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import project.comebackhomebe.domain.post.entity.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
