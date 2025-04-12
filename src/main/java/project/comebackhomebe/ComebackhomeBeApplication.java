e6f22c28f8c670033a8a59082c945b84cab36b18
HyeonSeong <gus8131@naver.com>
2025-04-12 10:41:23 +0900

    refactor : Swagger 문제 해결 1

--- src/main/java/project/comebackhomebe/ComebackhomeBeApplication.java
+++ src/main/java/project/comebackhomebe/ComebackhomeBeApplication.java
@@ -1,9 +1,12 @@
 package project.comebackhomebe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://cbh.kro.kr", description = "Default Server URL")})
 @SpringBootApplication
 @EnableJpaAuditing
 public class ComebackhomeBeApplication {
