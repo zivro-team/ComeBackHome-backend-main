b2c4c4fa1bbd08166e8ffbed58054ffa2d8f0020
HyeonSeong <gus8131@naver.com>
2025-04-18 15:08:20 +0900

    refactor : Security Config - 경로허가

--- src/main/java/project/comebackhomebe/global/security/config/SecurityConfig.java
+++ src/main/java/project/comebackhomebe/global/security/config/SecurityConfig.java
@@ -93,6 +93,7 @@ public class SecurityConfig {
                         .requestMatchers("/oauth2/**").permitAll()
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/dogInfo/**").permitAll()
 
                         // 나머지는 인증 필요
                         .anyRequest().authenticated()
