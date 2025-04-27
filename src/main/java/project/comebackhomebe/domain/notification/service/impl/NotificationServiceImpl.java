bf45e3bcde69fcc52fe5338791a2c53946ce86bb
HyeonSeong <gus8131@naver.com>
2025-04-27 13:46:14 +0900

    [#46] Feat : 알림 생성

--- src/main/java/project/comebackhomebe/domain/notification/service/impl/NotificationServiceImpl.java
+++ src/main/java/project/comebackhomebe/domain/notification/service/impl/NotificationServiceImpl.java
@@ -2,7 +2,9 @@ package project.comebackhomebe.domain.notification.service.impl;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import project.comebackhomebe.domain.notification.dto.request.NotificationRequest;
 import project.comebackhomebe.domain.notification.dto.response.NotificationResponse;
@@ -12,6 +14,7 @@ import project.comebackhomebe.domain.notification.service.NotificationService;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationServiceImpl implements NotificationService {
 
     private final NotificationRepository notificationRepository;
@@ -25,6 +28,22 @@ public class NotificationServiceImpl implements NotificationService {
         FirebaseMessaging.getInstance().send(notification.getFcmMessage());
 
         return NotificationResponse.of(notification);
+    }
+
+    @Override
+    public void sendMessage(String token, String title, String body) throws FirebaseMessagingException {
+        Message message = Message.builder()
+                .setToken(token)
+                .setNotification(com.google.firebase.messaging.Notification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .build())
+                .build();
+
+        String response = FirebaseMessaging.getInstance().send(message);
+
+        // 성공 로그 남기기
+        log.info("FCM 메시지 전송 성공, response: {}", response);
     }
 
 
