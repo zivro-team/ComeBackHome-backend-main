143ee211e4ed0ff59d6f0cdc8b2681be65821cbf
HyeonSeong <gus8131@naver.com>
2025-04-30 16:01:03 +0900

    [#50] Feat : 기능 구현 완료

--- src/main/java/project/comebackhomebe/admin/service/AdminService.java
+++ src/main/java/project/comebackhomebe/admin/service/AdminService.java
@@ -1,4 +1,16 @@
 package project.comebackhomebe.admin.service;
 
+import org.springframework.data.domain.Pageable;
+import project.comebackhomebe.domain.dog.dogInfo.dto.response.InfoResponse;
+import project.comebackhomebe.domain.dog.dogInfo.entity.Dog;
+import project.comebackhomebe.domain.member.dto.MemberInfo;
+
+import java.util.List;
+
 public interface AdminService {
+    // 회원정보 가져오기
+    List<MemberInfo> getMemberInfo(Pageable pageable);
+
+    // 강아지정보 가져오기
+    List<InfoResponse> getDogInfo(Pageable pageable);
 }
