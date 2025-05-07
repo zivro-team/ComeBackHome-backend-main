57b61b7fdb53cf595412b911162441f42557a32d
HyeonSeong <gus8131@naver.com>
2025-05-07 19:41:29 +0900

    [#62] Feat : 모든 API에 Rate Limit 걸기

--- src/main/java/project/comebackhomebe/domain/dog/dogInfo/controller/ApiV2DogController.java
+++ src/main/java/project/comebackhomebe/domain/dog/dogInfo/controller/ApiV2DogController.java
@@ -12,6 +12,7 @@ import project.comebackhomebe.domain.dog.dogInfo.dto.request.DogLostAllRequest;
 import project.comebackhomebe.domain.dog.dogInfo.dto.response.DogInfoResponse;
 import project.comebackhomebe.domain.dog.dogInfo.entity.Type;
 import project.comebackhomebe.domain.dog.dogInfo.service.DogService;
+import project.comebackhomebe.global.CustomRateLimiter.RateLimited;
 
 import java.io.IOException;
 import java.util.List;
@@ -25,6 +26,7 @@ public class ApiV2DogController {
     private final DogService dogService;
 
     // 정보 생성
+    @RateLimited
     @PostMapping("/lost")
     @Operation(summary = "강아지 정보 생성", description = "강아지 신고 LOST API 입니다.")
     public ResponseEntity<DogInfoResponse> createLostDogInfo(@RequestBody DogLostAllRequest dogAllRequest,
@@ -37,6 +39,7 @@ public class ApiV2DogController {
         ));
     }
 
+    @RateLimited
     @PostMapping("/discover")
     @Operation(summary = "강아지 정보 생성", description = "강아지 신고 LOST API 입니다.")
     public ResponseEntity<DogInfoResponse> createDiscoverDogInfo(@RequestBody DogDiscoverAllRequest dogAllRequest,
@@ -52,6 +55,7 @@ public class ApiV2DogController {
 
     //
     // 정보 가져오기
+    @RateLimited
     @GetMapping("/{id}")
     @Operation(summary = "특정 강아지 정보 가져오기", description = "특정 강아지 정보를 가져올때 사용하는 API")
     public ResponseEntity<DogInfoResponse> getDogInfo(@PathVariable("id") Long id) throws IOException {
@@ -59,6 +63,7 @@ public class ApiV2DogController {
     }
 
     // 정보 전체 가져오기
+    @RateLimited
     @GetMapping
     @Operation(summary = "강아지 전체 정보 가져오기", description = "강아지 정보 전체를 받아오는 API")
     public ResponseEntity<List<DogInfoResponse>> getAllDogInfo(Pageable pageable) throws IOException {
@@ -66,6 +71,7 @@ public class ApiV2DogController {
     }
 
     // 정보 수정하기
+    @RateLimited
     @PatchMapping("/{id}")
     @Operation(summary = "강아지 찾음으로 수정할때 사용", description = "강아지 수정으로 바꿈")
     public ResponseEntity<DogInfoResponse> updateDogInfo(@PathVariable("id") Long id) throws IOException {
@@ -74,6 +80,7 @@ public class ApiV2DogController {
 
 
     // 신고, 피신고 분류
+    @RateLimited
     @GetMapping("/type/{type}")
     @Operation(summary = "신고, 피신고 구분 강아지 정보 가져오기", description = "잃어버린 강아지와 발견한 강아지 따로 분류")
     public ResponseEntity<List<DogInfoResponse>> getDogInfoByType(@PathVariable("type") Type type,
@@ -82,6 +89,7 @@ public class ApiV2DogController {
     }
 
     // 종 분류
+    @RateLimited
     @GetMapping("/breed/{breed}")
     @Operation(summary = "강아지 종을 기준으로 분류", description = "강아지 종 별로 따로 보이게 분류")
     public ResponseEntity<List<DogInfoResponse>> getDogInfoByType(@PathVariable("breed") String breed,
@@ -90,6 +98,7 @@ public class ApiV2DogController {
     }
 
     // 정보 삭제하기
+    @RateLimited
     @DeleteMapping("/{id}")
     @Operation(summary = "강아지 정보 삭제", description = "강아지 정보를 삭제")
     public void deleteDogInfo(@PathVariable("id") Long id) throws IOException {
