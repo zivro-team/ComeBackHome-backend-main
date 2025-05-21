439532aeb2299205d961a464c24003dbf287bb86
HyeonSeong <gus8131@naver.com>
2025-05-21 11:30:34 +0900

    배포 오류 해결

--- src/main/java/project/comebackhomebe/domain/dog/dogImage/repository/DogImageRepository.java
+++ src/main/java/project/comebackhomebe/domain/dog/dogImage/repository/DogImageRepository.java
@@ -1,8 +1,10 @@
 package project.comebackhomebe.domain.dog.dogImage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.comebackhomebe.domain.dog.dogImage.entity.DogImage;
 
+@Repository
 public interface DogImageRepository extends JpaRepository<DogImage, Long> {
 
 }
