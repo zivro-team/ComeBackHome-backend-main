eaa701848adba905dde5943ffacbac2b4f364889
HyeonSeong <gus8131@naver.com>
2025-05-25 01:09:50 +0900

    [#82] Fix : Controller 분리

--- src/main/java/project/comebackhomebe/domain/chat/chatMessage/controller/ChatController.java
+++ src/main/java/project/comebackhomebe/domain/chat/chatMessage/controller/ChatController.java
@@ -1,27 +1,16 @@
 package project.comebackhomebe.domain.chat.chatMessage.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import project.comebackhomebe.domain.chat.chatMessage.dto.ChatMessageInfo;
 import project.comebackhomebe.domain.chat.chatMessage.service.ChatMessageService;
-import project.comebackhomebe.domain.chat.chatRoom.dto.ChatRoomInfo;
-import project.comebackhomebe.domain.chat.chatRoom.service.ChatRoomService;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
     private final ChatMessageService chatMessageService;
-    private final ChatRoomService chatRoomService;
 
     /**
      * 메시지 보내기
@@ -29,54 +18,8 @@ public class ChatController {
      * @param chatMessageinfo : sender, receiver, content
      */
     @MessageMapping("/chat")
+
     public void processMessage(@Payload ChatMessageInfo chatMessageinfo) {
         chatMessageService.processMessage(chatMessageinfo);
     }
-
-    /**
-     * 채팅방 들어가기
-     *
-     * @param senderId
-     * @param receiverId
-     * @return
-     */
-    @GetMapping("/messages/{senderId}/{receiverId}")
-    public ResponseEntity<List<ChatMessageInfo>> findChatMessage(
-            @PathVariable("senderId") String senderId,
-            @PathVariable("receiverId") String receiverId
-    ) {
-        return ResponseEntity.ok(chatMessageService.findChatMessage(senderId, receiverId));
-    }
-
-    /**
-     * 처음 채팅을 시작하고 싶을 때 사용하는 API
-     * 액세스 토큰과 dog 에서 두개의 id를 추출하여 채팅방 만듬
-     *
-     * @param request : 액세스 토큰
-     * @param dogId   : 게시글 Dog ID
-     * @return
-     */
-    @PostMapping("/chatRoom")
-    public ResponseEntity<ChatRoomInfo> findChatMessageSender(
-            HttpServletRequest request,
-            @RequestBody Long dogId
-    ) {
-        return ResponseEntity.ok(chatRoomService.createChatRooms(request, dogId));
-    }
-
-    /**
-     * 액세스 토큰으로 senderId로 변환하여
-     * 채팅방 리스트 보이게 만듬
-     *
-     * @param request : 액세스 토큰
-     * @return
-     */
-    @GetMapping("/list")
-    public ResponseEntity<List<ChatRoomInfo>> findChatRoom(
-            HttpServletRequest request
-    ) {
-        return ResponseEntity.ok(chatRoomService.getListChatRoom(request));
-    }
-
-
 }
---
+++ src/main/java/project/comebackhomebe/domain/chat/chatMessage/controller/ChatRestController.java
@@ -0,0 +1,68 @@
+package project.comebackhomebe.domain.chat.chatMessage.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.comebackhomebe.domain.chat.chatMessage.dto.ChatMessageInfo;
+import project.comebackhomebe.domain.chat.chatMessage.service.ChatMessageService;
+import project.comebackhomebe.domain.chat.chatRoom.dto.ChatRoomInfo;
+import project.comebackhomebe.domain.chat.chatRoom.service.ChatRoomService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/chat")
+@RequiredArgsConstructor
+public class ChatRestController {
+    private final ChatMessageService chatMessageService;
+    private final ChatRoomService chatRoomService;
+
+    /**
+     * 채팅방 들어가기
+     *
+     * @param senderId
+     * @param receiverId
+     * @return
+     */
+    @GetMapping("/messages/{senderId}/{receiverId}")
+
+    public ResponseEntity<List<ChatMessageInfo>> findChatMessage(
+            @PathVariable("senderId") String senderId,
+            @PathVariable("receiverId") String receiverId
+    ) {
+        return ResponseEntity.ok(chatMessageService.findChatMessage(senderId, receiverId));
+    }
+
+    /**
+     * 처음 채팅을 시작하고 싶을 때 사용하는 API
+     * 액세스 토큰과 dog 에서 두개의 id를 추출하여 채팅방 만듬
+     *
+     * @param request : 액세스 토큰
+     * @param dogId   : 게시글 Dog ID
+     * @return
+     */
+    @PostMapping("/chatRoom")
+    public ResponseEntity<ChatRoomInfo> findChatMessageSender(
+            HttpServletRequest request,
+            @RequestBody Long dogId
+    ) {
+        return ResponseEntity.ok(chatRoomService.createChatRooms(request, dogId));
+    }
+
+    /**
+     * 액세스 토큰으로 senderId로 변환하여
+     * 채팅방 리스트 보이게 만듬
+     *
+     * @param request : 액세스 토큰
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseEntity<List<ChatRoomInfo>> findChatRoom(
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.ok(chatRoomService.getListChatRoom(request));
+    }
+
+
+}
