c1e19a179e0237724355c54f3110970853278ad1
HyeonSeong <gus8131@naver.com>
2025-05-26 00:36:54 +0900

    [#82] Fix : WS COnfig 수정

--- src/main/java/project/comebackhomebe/domain/chat/config/WebSocketConfig.java
+++ src/main/java/project/comebackhomebe/domain/chat/config/WebSocketConfig.java
@@ -23,6 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
                 .setAllowedOriginPatterns("*");
     }
 
