1ffd18f1aaebe3015650ee423f39e154530d8732
HyeonSeong <gus8131@naver.com>
2025-05-28 23:02:14 +0900

    [#102] Refactor : 알림 로직 주석처리 및 최적화

--- src/main/java/project/comebackhomebe/domain/notification/entity/Notification.java
+++ src/main/java/project/comebackhomebe/domain/notification/entity/Notification.java
@@ -1,30 +1,66 @@
 package project.comebackhomebe.domain.notification.entity;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
 
 @Document(indexName = "notification")
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notification {
 
+    /**
+     * 알림 고유 ID
+     */
     @Id
     private String id;
 
-    private String token; // 안드로이드에서 제공받는 토큰
+    /**
+     * 알림 제목
+     */
+    @Field(type = FieldType.Text)
+    private String title;
 
-    private String title; // 제목
+    /**
+     * 알림 내용
+     */
+    @Field(type = FieldType.Text)
+    private String message;
 
-    private String message; // 해당 메시지
+    /**
+     * FCM 토큰
+     */
+    @Field(type = FieldType.Text)
+    private String token;
 
+    /**
+     * 알림 시간
+     */
+    @Field(name = "@timestamp", type = FieldType.Date)
+    private LocalDateTime timestamp;
+
+    /**
+     * 알림 빌더 형식 생성
+     * @param token : FCM 토큰
+     * @param title : 강아지 발견!
+     * @param message : 주위에서 강아지가 발견되었어요!
+     * @return
+     */
     public static Notification from(String token, String title, String message) {
         return Notification.builder()
                 .token(token)
                 .title(title)
                 .message(message)
+                .timestamp(LocalDateTime.now())
                 .build();
     }
 }
--- src/main/java/project/comebackhomebe/domain/notification/repository/NotificationRepository.java
+++ src/main/java/project/comebackhomebe/domain/notification/repository/NotificationRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import project.comebackhomebe.domain.notification.entity.Notification;
 
 public interface NotificationRepository extends ElasticsearchRepository<Notification, String> {
-    // 이거 ELK 적용해야할듯
 }
